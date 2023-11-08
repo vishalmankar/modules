913f4abd0ee7c9525094a3bb645d871a3ba5dcce
vishalmankar <124581233+vishalmankar@users.noreply.github.com>
2023-11-08 20:01:42 +0530

    Create resource.tf

---
+++ resource.tf
@@ -0,0 +1,10 @@
+resource "aws_key_pair" "key-tf" {
+  key_name = var.key_name
+  public_key = var.key
+}
+
+resource "aws_instance" "web" {
+  ami                    = var.image_id
+  instance_type          = var.instance_type
+  key_name = "${aws_key_pair.key-tf.key_name}"
+}
